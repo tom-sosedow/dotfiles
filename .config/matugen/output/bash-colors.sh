5e8f002df71631d66aa5451e9d6885d9a96dd450
Tom Sosedow <tom.sosedow@gmail.com>
2025-03-09 19:05:42 +0100

    add invert colors shader

--- .config/matugen/output/bash-colors.sh
+++ .config/matugen/output/bash-colors.sh
@@ -3,28 +3,28 @@
 wallpaper='/home/tom/Bilder/Hintergründe/wallpaper-main/wallhaven-l8q85r.jpg'
 
 # Special
-background='#161217'
-foreground='#e9e0e7'
-cursor='#e1b8f5'
+background='#0e1514'
+foreground='#dde4e3'
+cursor='#80d4d5'
 
 # Colors
-color0='#161217'
-color1='#e1b8f5'
-color2='#d3c0d8'
-color3='#f4b7b8'
-color4='#f5d9ff'
-color5='#efdcf5'
-color6='#ffdada'
-color7='#e9e0e7'
+color0='#0e1514'
+color1='#80d4d5'
+color2='#b0cccb'
+color3='#b3c8e9'
+color4='#9cf1f1'
+color5='#cce8e7'
+color6='#d3e3ff'
+color7='#dde4e3'
 
-color8='#161217'
-color9='#422255'
-color10='#382c3e'
-color11='#4c2527'
-color12='#2b0b3f'
-color13='#221728'
-color14='#331113'
-color15='#e9e0e7'
+color8='#0e1514'
+color9='#003737'
+color10='#1b3435'
+color11='#1d314b'
+color12='#002020'
+color13='#041f20'
+color14='#051c35'
+color15='#dde4e3'
 
 # FZF colors
 export FZF_DEFAULT_OPTS="
